21ed59753a00db4c459c3cf1470d56ac426e6e42
Gary Cruise <Garycruisee@gmail.com>
2023-07-19 02:12:28 +0700

    chat & communitydetail & css

--- src/components/UserCards.tsx
+++ src/components/UserCards.tsx
@@ -8,7 +8,7 @@ export type UserCardsProps = {
 
 export function UserCards(props: UserCardsProps) {
   return (
-    <Link href={`/profile/${props.id}`}>
+    <Link href={`/community/${props.id}`}>
       <div className="max-w-xs text-white rounded border shadow-xl shadow-black hover:scale-110 duration-300">
         <img src="/profile-picture.jpg"></img>
         <div className="flex justify-between px-3 py-1 items-center bg-gradient-to-r from-stone-500 to-stone-800">
